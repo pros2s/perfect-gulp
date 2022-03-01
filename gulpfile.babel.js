a3b96d044ccf6deded336d178dd69729a9c056ac
Alexandr <caiiiia_podoplelov@mail.ru>
2022-03-01 22:03:21 +0300

    scripts is done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,14 +2,17 @@ import gulp from 'gulp';
 import clean from './gulp/tasks/clean';
 import server from './gulp/tasks/server';
 import config from './gulp/config';
+import { scriptsBuild, scriptsWatch } from './gulp/tasks/scripts';
 
 config.setEnv();
 
 export const build = gulp.series(
   clean,
+  scriptsBuild,
 );
 
 export const watch = gulp.series(
   build,
   server,
+  scriptsWatch,
 );
